84e07be2d2609607f4640f77324e19eaeb54e272
Churycheva Nataliia <churycheva.nataliia@gmail.com>
2023-05-09 10:49:01 +0200

    adding-the-new-meetup-form

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,12 @@
 //our-domain.com/new-meetup
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+function NewMeetupPage() {
+	function addMeetupHandler( meetupData ) {
+		console.log(meetupData);
+	}
+
+	return <NewMeetupForm onAddMeetup={ addMeetupHandler } />;
+}
+
+export default NewMeetupPage;
