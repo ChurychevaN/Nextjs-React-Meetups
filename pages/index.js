f24da766fab9bc2661ed13b8fcda6cf2230d8c73
Churycheva Nataliia <churycheva.nataliia@gmail.com>
2023-05-10 12:06:15 +0200

    server-side-rendering

--- pages/index.js
+++ pages/index.js
@@ -28,29 +28,29 @@ function HomePage( props ) {
 	);
 }
 
-export async function getServerSideProps(context) {
-	const req = context.req;
-	const res = context.res;
+// export async function getServerSideProps(context) {
+// 	const req = context.req;
+// 	const res = context.res;
+//
+// //fetch data from an API
+//
+// 	return {
+// 		props:{
+// 			meetups: DUMMY_MEETUPS
+// 		}
+// 	}
+// }
 
-//fetch data from an API
+export async function getStaticProps() {
+
+// fetch data from an API
 
 	return {
-		props:{
-			meetups: DUMMY_MEETUPS
-		}
-	}
+		props: {
+			meetups: DUMMY_MEETUPS,
+		},
+		revalidate: 1,
+	};
 }
-
-// export async function getStaticProps() {
-
-//fetch data from an API
-
-// 	return {
-// 		props: {
-// 			meetups: DUMMY_MEETUPS,
-// 		},
-// 		revalidate: 1,
-// 	};
-// }
 
 export default HomePage;
