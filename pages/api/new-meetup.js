438f9d48c7bd98ab557c725b46f1b823d48d1442
Churycheva Nataliia <churycheva.nataliia@gmail.com>
2023-05-11 14:43:41 +0200

    getting-data-from-database

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -4,6 +4,7 @@ import { MongoClient } from 'mongodb';
 async function handler( req, res ) {
 	if (req.method === 'POST') {
 		const data = req.body;
+
 		const client = await MongoClient.connect('mongodb://localhost:27017');
 		const db = client.db();
 
